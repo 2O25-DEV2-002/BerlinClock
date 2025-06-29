dfc32f795dc43ddc1df427ff49f1e0415bb89ef7
Anonymous <anonymous.unknown2508@gmail.com>
2025-06-29 23:01:17 +0530

    Refactor the getSeconds test class with Truth assertion for exception message

--- app/src/test/java/com/anonymous/berlinclock/domain/usecase/GetBerlinClockDataTest.kt
+++ app/src/test/java/com/anonymous/berlinclock/domain/usecase/GetBerlinClockDataTest.kt
@@ -1,25 +1,28 @@
 package com.anonymous.berlinclock.domain.usecase
 
+import com.google.common.truth.Truth.assertThat
 import org.junit.Assert.assertThrows
 import org.junit.Test
 
 class GetBerlinClockDataTest {
 
-    private val getBerlinClockData = GetBerlinClockData()
+    private lateinit var getBerlinClockData: GetBerlinClockData
 
     @Test
     fun `getSeconds throws exception when input is negative`() {
+        getBerlinClockData = GetBerlinClockData()
         val exception = assertThrows(IllegalArgumentException::class.java) {
             getBerlinClockData.getSeconds(-1)
         }
-        assert(exception.message!!.contains("Seconds should be 0 or greater"))
+        assertThat(exception).hasMessageThat().contains("Seconds should be 0 or greater")
     }
 
     @Test
     fun `getSeconds throws exception when input is greater than 59`() {
+        getBerlinClockData = GetBerlinClockData()
         val exception = assertThrows(IllegalArgumentException::class.java) {
             getBerlinClockData.getSeconds(60)
         }
-        assert(exception.message!!.contains("Seconds should be 0 or greater"))
+        assertThat(exception).hasMessageThat().contains("Seconds should be 0 or greater")
     }
 }
