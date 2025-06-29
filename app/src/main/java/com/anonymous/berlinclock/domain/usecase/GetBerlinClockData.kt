e7f13907cbd5ddd7677764591e8cca4f9e197a24
Anonymous <anonymous.unknown2508@gmail.com>
2025-06-29 23:19:28 +0530

    Refactor getSeconds function in GetBerlinClockData use-case

--- app/src/main/java/com/anonymous/berlinclock/domain/usecase/GetBerlinClockData.kt
+++ app/src/main/java/com/anonymous/berlinclock/domain/usecase/GetBerlinClockData.kt
@@ -5,11 +5,12 @@ import com.anonymous.berlinclock.util.MESSAGE_INPUT_BETWEEN_0_AND_59
 import com.anonymous.berlinclock.util.SecondLamp
 import com.anonymous.berlinclock.util.TIME_MAX_VALUE
 import com.anonymous.berlinclock.util.TIME_MIN_VALUE
+import com.anonymous.berlinclock.util.isEven
 
 class GetBerlinClockData {
 
     fun getSeconds(seconds: Int): SecondLamp {
         require(seconds in TIME_MIN_VALUE..TIME_MAX_VALUE) { MESSAGE_INPUT_BETWEEN_0_AND_59 }
-        return if (seconds % 2 == 0) LampColour.YELLOW else LampColour.OFF
+        return if (seconds.isEven()) LampColour.YELLOW else LampColour.OFF
     }
 }
---
+++ app/src/main/java/com/anonymous/berlinclock/util/IntExtensions.kt
@@ -0,0 +1,3 @@
+package com.anonymous.berlinclock.util
+
+fun Int.isEven() = this % 2 == 0
